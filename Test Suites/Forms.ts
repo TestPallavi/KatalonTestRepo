2f920c6802577ee41f940920207a6025669c8b3f
shibyala_pallavi <shibyala_pallavi@PSL-GQWWX93.persistent.co.in>
2024-01-23 12:46:25 +0530

    THiRD Commit

--- Test Suites/Forms.ts
+++ Test Suites/Forms.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>aedb3959-1ec9-4cf1-a0da-8f955c870a30</testSuiteGuid>
+   <testCaseLink>
+      <guid>bd63bc34-2445-4950-9480-7e9161790e15</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Forms/Forms Submit</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
